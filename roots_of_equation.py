d56119212c76d3ca736f594e5cee6ff2abecad2a
1996Shubh <modishubh29@gmail.com>
2020-02-10 11:05:08 +0530

    Added eight program

---
+++ roots_of_equation.py
@@ -0,0 +1,18 @@
+a=int(input("enter the value of a in a*x*x+b*x+c:"))
+b=int(input("enter the value of b in a*x*x+b*x+c:"))
+c=int(input("enter the value of c in a*x*x+b*x+c:"))
+
+def roots_of_equation(a,b,c):
+    delta=b*b-4*a*c
+    if delta>=0:
+        root1=round((-b+(delta**0.5))/(2*a),4)
+        root2=round((-b-(delta**0.5))/(2*a),4)
+        print(f"root1={root1}")
+        print(f"root2={root2}")
+
+    else:
+        print(f"root1=({-b}+{round((-1*delta)**0.5,4)}i)/{2*a}")
+        print(f"root2=({-b}-{round((-1*delta)**0.5,4)}i)/{2*a}")
+
+
+roots_of_equation(a,b,c)
